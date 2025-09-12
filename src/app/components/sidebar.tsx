af58dec00b82b36a42f352c3c34c903dad9a86fb
dotbillu <billudestroyer@outlook.com>
2025-09-13 00:56:32 +0530

    initial

---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,7 @@
+"use client";
+export default function sidebar() {
+  return (
+         <div className="fixed top-0 left-0 m-2 shadow shadow-gray-50 w-[23%] h-[70%] rounded-[6px]">
+ earth</div>
+  );
+}
