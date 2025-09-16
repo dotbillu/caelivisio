27ec572dc1e9de79c0c93b65e517884b194935ef
dotbillu <billudestroyer@outlook.com>
2025-09-16 23:46:32 +0530

    added iss rendering

--- src/app/components/earth.tsx
+++ src/app/components/earth.tsx
@@ -6,26 +6,34 @@ import { useEffect } from "react";
 import { Entity } from "resium";
 import SatelliteData from "../libs/satellites";
 import { useAtom } from "jotai";
-import { satelliteObjectAtoms } from "./store";
+import { satelliteObjectAtom, SpaceStationObjectAtom } from "./store";
 const Viewer = dynamic(() => import("resium").then((mod) => mod.Viewer), {
   ssr: false,
 });
 export default function Earth() {
   const cesiumtoken = process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN;
-  const [satelliteData, setSateliteData] = useAtom(satelliteObjectAtoms);
+  const [satelliteData, setSateliteData] = useAtom(satelliteObjectAtom);
+  const [spaceStation, setSpaceStation] = useAtom(SpaceStationObjectAtom);
+
   useEffect(() => {
     Ion.defaultAccessToken = cesiumtoken!;
 
     async function getSatelliteTLE() {
       const SatelliteURL =
         "https://celestrak.org/NORAD/elements/gp.php?GROUP=last-30-days&FORMAT=tle";
-      const res = await axios.get(SatelliteURL, { responseType: "text" });
-      const Satellites=SatelliteData(res.data);
+      let res = await axios.get(SatelliteURL, { responseType: "text" });
+      const Satellites = SatelliteData(res.data);
       setSateliteData(Satellites);
+      const SpaceStationURL =
+        "https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle";
+      res = await axios.get(SpaceStationURL, { responseType: "text" });
+      const spaceStations = SatelliteData(res.data);
+      setSpaceStation(spaceStations);
     }
 
     getSatelliteTLE();
-  }, [cesiumtoken, setSateliteData]);
+  }, [cesiumtoken, setSateliteData, setSpaceStation]);
+
   return (
     <div>
       <Viewer full>
@@ -36,6 +44,14 @@ export default function Earth() {
             position={Cartesian3.fromDegrees(sat.lng, sat.lat, sat.height)}
             point={{ pixelSize: 5, color: Color.YELLOW }}
           />
+        ))}
+        {spaceStation.map((sat, idx) => (
+          <Entity
+            key={idx}
+            name={`Satellite ${idx}`}
+            position={Cartesian3.fromDegrees(sat.lng, sat.lat, sat.height)}
+            point={{ pixelSize: 10, color: Color.GREEN }}
+          ></Entity>
         ))}
       </Viewer>
     </div>
